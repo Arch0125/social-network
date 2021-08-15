6b657e827c4960e3887c00d8b36915c2c7e34690
Arch0125 <you@example.com>
2021-08-15 15:01:27 +0530

    Updates

---
+++ precache-manifest.ddefa11dac6c8d77e3e6a0f08e5bf927.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a99e3ce8b7a24e05ca7a",
+    "url": "/social-network/static/css/main.fa1412cf.chunk.css"
+  },
+  {
+    "revision": "a99e3ce8b7a24e05ca7a",
+    "url": "/social-network/static/js/main.a99e3ce8.chunk.js"
+  },
+  {
+    "revision": "733186d0a85ff80c96eb",
+    "url": "/social-network/static/css/1.bb0601c7.chunk.css"
+  },
+  {
+    "revision": "733186d0a85ff80c96eb",
+    "url": "/social-network/static/js/1.733186d0.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "69ef283378d9564f920d90833d145844",
+    "url": "/social-network/index.html"
+  }
+];
